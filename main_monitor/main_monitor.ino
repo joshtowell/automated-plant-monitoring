e31a9a4ab93cd668e6d0c7b8a7042ef4930b72b2
Joshua Towell <jntowell@hotmail.com>
2021-07-17 20:27:00 +0100

    Added comments to all code

--- main_monitor/main_monitor.ino
+++ main_monitor/main_monitor.ino
@@ -2,8 +2,8 @@
 
 const int pinDHT11 = 2;
 
-int wetVal = 262;
-int dryVal = 571;
+int wetStored = 262;
+int dryStored = 571;
 int moistLow = 0;
 int moistLowCount = 0;
 int moistHigh = 0;
@@ -18,7 +18,9 @@ int moistHighCount = 0;
 SimpleDHT11 dht11(pinDHT11);
 
 void setup() {
+  // Intialise serial connection
   Serial.begin(9600);
+  // Wait for sensors to intialise
   Serial.println("Initializing sensors...");
   Serial.println();
   delay(3000);
@@ -26,7 +28,7 @@ void setup() {
 
 void loop() {
   senseTempHumid();
-  senseMoist();
+  senseMoist(wetStored, dryStored);
   Serial.println();
 
   // DHT11 sampling rate is 1HZ
@@ -37,6 +39,8 @@ void senseTempHumid() {
   byte rawTemp = 0;
   byte rawHumid = 0;
   int err = SimpleDHTErrSuccess;
+  // Read raw digital sensor data
+  // Catch sensor exceptions and display errors
   if ((err = dht11.read(&rawTemp, &rawHumid, NULL)) != SimpleDHTErrSuccess) {
     Serial.print("Read DHT11 failed, err=");
     Serial.print(SimpleDHTErrCode(err));
@@ -52,35 +56,43 @@ void senseTempHumid() {
   }
 }
 
-void senseMoist() {
+void senseMoist(int wetLim, int dryLim) {
+  // Read raw analog sensor data
   int rawMoist = analogRead(A0);
-  int percentMoist = map(rawMoist, wetVal, dryVal, 100, 0);
+  // Map value as percentage using known limits
+  int percentMoist = map(rawMoist, wetLim, dryLim, 100, 0);
   Serial.print(percentMoist);
   Serial.println("%");
 
   // Auto-calibrate 10 continuous low moisture events
+  // If below 0% trigger low event
   if (percentMoist < 0) {
+    // Store event details
     moistLow = percentMoist;
     moistLowCount++;
+    // If 10 events, then set new low limit
     if (moistLowCount > 9 && percentMoist == moistLow) {
       Serial.print("New low moisture detected at ");
       Serial.print(percentMoist);
       Serial.println("%");
-      dryVal = rawMoist;
+      dryStored = rawMoist;
       moistLowCount = 0;
       Serial.println("Low moisture limit successfully re-calibrated");
     }
   }
 
   // Auto-calibrate 10 continuous high moisture events
+  // If above 100% trigger high event
   if (percentMoist > 100) {
+    // Store event details
     moistHigh = percentMoist;
     moistHighCount++;
+    // If 10 events, then set new low limit
     if (moistHighCount > 9 && percentMoist == moistHigh) {
       Serial.print("New high moisture detected at ");
       Serial.print(percentMoist);
       Serial.println("%");
-      wetVal = rawMoist;
+      wetStored = rawMoist;
       moistHighCount = 0;
       Serial.println("High moisture limit successfully re-calibrated");
     }
